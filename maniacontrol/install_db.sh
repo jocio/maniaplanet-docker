a7059677cf2b82c9c2dd84ba540fe983311cf93c
jocio <63001761+jocio@users.noreply.github.com>
2020-04-05 22:38:42 +0200

    User accessible from everywhere

--- maniacontrol/install_db.sh
+++ maniacontrol/install_db.sh
@@ -13,8 +13,8 @@ rm -f ${SQL_FILE_NAME}
 
 echo "Creating SQL script ..."
 echo "CREATE DATABASE ${DATABASE_NAME} DEFAULT CHARACTER SET 'utf8mb4' DEFAULT COLLATE 'utf8mb4_unicode_ci';" >> ${SQL_FILE_NAME}
-echo "CREATE USER '${USER_NAME}'@'localhost' IDENTIFIED BY '${USER_PASSWORD}';" >> ${SQL_FILE_NAME}
-echo "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.*  TO '${USER_NAME}'@'localhost';" >> ${SQL_FILE_NAME}
+echo "CREATE USER '${USER_NAME}'@'%' IDENTIFIED BY '${USER_PASSWORD}';" >> ${SQL_FILE_NAME}
+echo "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.*  TO '${USER_NAME}'@'%';" >> ${SQL_FILE_NAME}
 
 echo "Executing SQL script as root ..."
 sudo mysql -h ${DATABASE_HOST} -u root -p < ${SQL_FILE_NAME}
